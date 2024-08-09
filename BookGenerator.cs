d01d50682ac5d6890b79db0e317d947227c1ee09
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-08-09 03:08:28 +0100

    Book Title

--- BookGenerator.cs
+++ BookGenerator.cs
@@ -3,13 +3,15 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Windows.Forms.VisualStyles;
 
 public class RandomTextGenerator
 {
     private static Random random = new Random();
+    private static string title = string.Empty;
     public static void GenerateTextFile(int titleLength, int contentLength)
     {
-        var title = GenerateRandomText(titleLength, true);
+        title = GenerateRandomText(titleLength, true);
         var filename = $"{title}.txt";
         var content = GenerateRandomText(contentLength, false);
         File.WriteAllText(filename, content);
@@ -19,6 +21,11 @@ public class RandomTextGenerator
         var randomText = new StringBuilder();
         int lineLength = 0;
         int lineNumber = 0;
+        if (!isFileName)//write title as first line and then append two new lines
+        {
+            randomText.Append($"{title}");
+            randomText.Append("\n\n");
+        }
         for (var i = 0; i < length; i++)
         {
             var codePoint = random.Next(0x20, 0x10ffff); // valid Unicode code points
@@ -37,11 +44,11 @@ public class RandomTextGenerator
                 randomText.Append("\n");
                 lineLength = 0; // update line length counter
                 lineNumber += 1; // update line number counter
-                if (lineNumber == 40) // insert newline every 40 lines
-                {
-                    randomText.Append("\n");
-                    lineNumber = 0; // reset line number counter
-                }
+            }
+            if (lineNumber == 40) // insert newline every 40 lines
+            {
+                randomText.Append("\n");
+                lineNumber = 0; // reset line number counter
             }
             randomText.Append(charString);
             lineLength += charLength;

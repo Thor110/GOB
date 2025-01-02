de823d578fb2054464a87a59e051c301e42dfeee
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-01-02 04:09:14 +0000

    comment

--- BookGenerator.cs
+++ BookGenerator.cs
@@ -2,7 +2,7 @@
 public class RandomTextGenerator
 {
     private static Random random = new Random();
-    private static string title = string.Empty; // title is stored as a variable so that it can be added to the first line of the text file
+    private static string title = string.Empty;
     public static void GenerateTextFile(int titleLength, int contentLength, int lineLength, int paragraphLength)
     {
         title = GenerateRandomText(titleLength, true, 0, 0);

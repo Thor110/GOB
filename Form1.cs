580921d5d8b841a8d750374696a45e57c1f32f40
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-01-10 02:47:15 +0000

    Commented Code Removed

--- Form1.cs
+++ Form1.cs
@@ -372,13 +372,8 @@
         {
             if (comboBox1.SelectedIndex >= 60 && comboBox1.SelectedIndex <= 62) // Surrogate Code Points Dictionary Entries
             {
-                //surrogates = true;
                 MessageBox.Show("Surrogate code points cannot be used in file names, so ASCII will be used instead and the text inside the file will contain surrogate pairs.");
-            }/*
-            else
-            {
-                surrogates = false;
-            }*/
+            }
             var selectedLanguage = (string)comboBox1.SelectedItem;
             var range = unicodeRanges.First(x => x.Value.language == selectedLanguage).Value;
             richTextBox1.Text = range.description;
